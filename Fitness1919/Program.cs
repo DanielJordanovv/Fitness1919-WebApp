ea7ac00e2e684e235d34d37b74cac5fa641778fb
DanielJordanovv <d.jordanov.dev@gmail.com>
2023-07-10 16:55:53 +0300

    Login Register and Home page changes

--- Fitness1919/Program.cs
+++ Fitness1919/Program.cs
@@ -9,7 +9,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlServer(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
-
 builder.Services.AddDefaultIdentity<IdentityUser>(options =>
 {
     options.SignIn.RequireConfirmedAccount = false;
