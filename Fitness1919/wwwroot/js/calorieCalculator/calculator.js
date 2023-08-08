c47a753b4eb42b8a30c2ed6fdcbfdb1ff6896f43
Daniel Jordanov <114871054+DanielJordanovv@users.noreply.github.com>
2023-08-08 17:25:02 +0300

    js abstraction

---
+++ Fitness1919/wwwroot/js/calorieCalculator/calculator.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('.calorieForm').submit(function (event) {
+        event.preventDefault();
+
+        $.ajax({
+            url: '@Url.Action("Calculate", "CalorieCalculator")',
+            type: 'POST',
+            data: $(this).serialize(),
+            success: function (result) {
+                $('#result').html(result);
+            },
+            error: function (xhr) {
+                console.log(xhr.responseText);
+                alert('An error occurred while calculating the calories. Please try again.');
+            }
+        });
+    });
+});
